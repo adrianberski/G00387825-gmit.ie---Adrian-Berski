f863a074e187d9d0aec81af5d973bcd083dda7f9
Adrian <60187350+adrianberski@users.noreply.github.com>
2020-03-31 14:23:30 -0400

    Update python_plot.py

--- python_plot.py
+++ python_plot.py
@@ -7,42 +7,44 @@ import numpy as np
 
 
 # Create functions and using data
+#x = [0, 1, 2, 3, 4] or x = range(4)
+#f = [0, 1, 2, 3, 4]
+#g = [0, 1, 4, 9, 16] - from function gf
+#h = [0, 1, 8, 27, 64] - from functiongf
+
+
+def gf(a, b, c, d, e):
+    f = a**2
+    s = b**2
+    t = c**2
+    fo = d**2
+    fi = e**2
+    return f, s, t, fo, fi
+
+
+
+def hf(a, b, c, d, e):
+    f = a**3
+    s = b**3
+    t = c**3
+    fo = d**3
+    fi = e**3
+    return f, s, t, fo, fi
+
+
 x = [0, 1, 2, 3, 4]
 f = [0, 1, 2, 3, 4]
-g = [0, 1, 4, 9, 16]
-h = [0, 1, 8, 27, 64]
-
-#def g(a, b, c, d, e):
- #   f = a**2
-  #  s = b**2
-   # t = c**2
-    #fo = d**2
-    #fi = e**2
-    #return f, s, t, fo, fi
-#print (g(0, 1, 2, 3, 4))
-
-
-#def h(a, b, c, d, e):
-    #f = a**3
-    #s = b**3
-    #t = c**3
-    #fo = d**3
-    #fi = e**3
-    #return f, s, t, fo, fi
-#print (h(0, 1, 2, 3, 4))
-
-
-#def h(x):
-    #return (x) * 3
+g = gf(0, 1, 2, 3, 4)
+h = hf(0, 1, 2, 3, 4)
 
 
 
-#create plots with above data
+#create plots with above data on the one set of axes
 plt.plot (x, f)
 plt.plot (x, g)
 plt.plot (x, h)
 
-#swow plots for 3 functions 
+#swow plots for 3 functions on the one set of axes
 plt.show()
 
 
